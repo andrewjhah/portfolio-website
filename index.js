533d72a9277248cd13c075d9e9df33046c17d617
Andrew Hah <andrew.hah@gmail.com>
2023-05-01 15:52:38 -0400

    updates

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
